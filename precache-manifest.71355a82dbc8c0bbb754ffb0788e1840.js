614143296ae65d3da7adc1a58a800ac3a73ff8d5
dinhmai74 <anh.dinh74@gmail.com>
2020-01-10 12:05:41 +0700

    Updates

---
+++ precache-manifest.71355a82dbc8c0bbb754ffb0788e1840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30bc0ef248d294a16a900ea3d51893c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "162df03e95f1bbf5df65",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f8ef11defcb3558664e",
+    "url": "/my-app/static/js/2.9d2a95c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.9d2a95c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "162df03e95f1bbf5df65",
+    "url": "/my-app/static/js/main.ac706707.chunk.js"
+  },
+  {
+    "revision": "44048a88bd550b975a49",
+    "url": "/my-app/static/js/runtime-main.f451602f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
